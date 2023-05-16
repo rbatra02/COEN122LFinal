03deb6dc03c0ab660599a24f2c483ac0cd664b3a
Will Maddrey <williammaddrey@gmail.com>
2023-05-16 16:35:49 -0700

    Control Block
    Added control block file

---
+++ finalproject.srcs/sources_1/new/controlBlock.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05/16/2023 04:35:30 PM
+// Design Name: 
+// Module Name: controlBlock
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module controlBlock(
+
+    );
+endmodule
